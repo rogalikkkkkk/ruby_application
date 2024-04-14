53750ca8ada00e1e7af8f76cf75e396a11fbfcb2
rogalikkkkkk <dmitriy.zharkov.2001@yandex.ru>
2024-04-14 22:28:46 +0300

    Second work commit

--- app/javascript/application.js
+++ app/javascript/application.js
@@ -1,3 +1,12 @@
 // Configure your import map in config/importmap.rb. Read more: https://github.com/rails/importmap-rails
 import "@hotwired/turbo-rails"
 import "controllers"
+
+//= require_self
+//= require jquery
+//= require jquery-ui
+//= require jquery_ujs
+//= require turbolinks
+//= require bootstrap
+
+//= require_tree .
